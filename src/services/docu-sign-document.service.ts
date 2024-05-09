b9b33f0bf5784a598ca0668180b34b71a6a90284
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2024-05-09 23:24:17 +0800

    Refactor Docusign module and service, update file structure and dependencies

--- src/services/docu-sign-document.service.ts
+++ src/services/docu-sign-document.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { DocusignModuleOptions } from '../interfaces/docusign-options.interface';
+import * as docusign from 'docusign-esign';
 
+/**
+ * Service responsible for handling document operations
+ */
 @Injectable()
-export class DocuSignDocumentService {}
+export class DocuSignDocumentService {
+  private readonly logger = new Logger(DocuSignDocumentService.name);
+  private apiClient: docusign.ApiClient;
+
+  constructor(@Inject('DOCUSIGN_CONFIG') private config: DocusignModuleOptions) {
+    this.apiClient = new docusign.ApiClient();
+    this.apiClient.setOAuthBasePath(this.config.oAuthBasePath);
+    this.apiClient.setBasePath(this.config.basePath);
+  }
+
+  /**
+   * Get document by its ID
+   * @param documentId
+   * @returns
+   */
+  async getDocument(documentId: string): Promise<docusign.EnvelopeDocument> {
+    this.logger.log('Getting document...');
+    try {
+      const results = await this.apiClient.getEnvelopeDocument(
+        this.config.accountId,
+        documentId,
+      );
+      return results;
+    } catch (error) {
+      this.logger.error('Failed to get document:', error);
+      throw error;
+    }
+  }
+}
