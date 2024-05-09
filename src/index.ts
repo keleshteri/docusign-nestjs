d7b5c32adace0726f0378c17397896fb66c61973
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2024-05-09 15:51:41 +0800

    chore: Update Docusign module and service, bump version to 1.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { DocusignModule } from "./docusign.module";
 import { DocusignService } from "./services/docusign.service";
+import { DocuSignConfigService } from './services/docu-sign-config.service';
+import { DocuSignAuthenticationService } from './services/docu-sign-authentication.service';
 
-export { DocusignModule, DocusignService };
+export {
+  DocusignModule,
+  DocusignService,
+  DocuSignConfigService,
+  DocuSignAuthenticationService,
+};
